95003a2dabe1f990f1c2d42057069cf0ae309357
Hugh Luscombe <hughluscombe@MacBook-Pro.local>
2019-04-02 16:31:39 +1100

    rough game logic working

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -20,8 +20,7 @@ const winCombos = [
 
 console.log(winCombos[1][2]);
 
-
-$( document ).ready( function() {
+$(document).ready(function() {
   //startGame();
 
   let player1TileArray = [];
@@ -39,37 +38,38 @@ $( document ).ready( function() {
 
   const checkCombos = function() {
     for (var i = 0; i < winCombos.length; i++) {
-      if ( player1TileArray == (winCombos[i][0] && winCombos[i][1] && winCombos[i][2]) ) {
-        alert(`PLayer 1 is the winner`);
+      if ( player1TileArray.includes(winCombos[i][0]) && player1TileArray.includes(winCombos[i][1]) && player1TileArray.includes(winCombos[i][2]) ) {
+        alert(`Player 1 is the winner`);
+        $('.playerturn').html("GAME OVER");
         break;
-      } else if ( player2TileArray == (winCombos[i][0] && winCombos[i][1] && winCombos[i][2]) ) {
-        alert(`PLayer 2 is the winner`);
+        // clear board? trigger animation
+      } else if ( player2TileArray.includes(winCombos[i][0]) && player2TileArray.includes(winCombos[i][1]) && player2TileArray.includes(winCombos[i][2]) ) {
+        alert(`Player 2 is the winner`);
+        $('.playerturn').html("GAME OVER");
         break;
-      } else
-        console.log("no match");
-        break;
+        //clear board? trigger animation
       }
-    };
+    }
   };
 
   $('.tile').on('click', function(event) {
 
     let clickedTile = Number(event.target.id);
-    $(this).html(turn);
+    $(this).html(turn).off('click');
 
     if (turn == player1) {
-      player1TileArray.push(event.target.id);
+      player1TileArray.push(clickedTile);
+      player1TileArray.sort();
     } else {
-      player2TileArray.push(event.target.id);
+      player2TileArray.push(clickedTile);
+      player2TileArray.sort();
     }
     console.log(player1TileArray);
     console.log(player2TileArray);
 
     if (player1TileArray.length >= 3) {
-      console.log('there is 3');
       checkCombos();
     } else if (player2TileArray.length >= 3) {
-      console.log('there is 3');
       checkCombos();
     }
 
@@ -78,20 +78,24 @@ $( document ).ready( function() {
 
 
 
-// iterate through turns - display players turn?
+  // iterate through turns - display players turn?
 
-// log id of clicked sqaure and add to array for player in js
+  // log id of clicked sqaure and add to array for player in js
 
-//stop buttons from being able to double click
+  //stop buttons from being able to double click
 
-// once one players turns have reached 3, check combination of classes for winning combo with for loop
+  // once one players turns have reached 3, check combination of classes for winning combo with for loop
 
-// if there is no match continue and check for win next turn
+  // if there is no match continue and check for win next turn
 
-// if there is a match trigger animation? reset prompt? link to youtube?
+  // if there is a match trigger animation? reset prompt? link to youtube?
 
-// winner = true {reset game button appears}
+  // winner = true {reset game button appears}
 
-// let winner = ;
-// winMessage.html(`${winner} is the winner`);
+  // let winner = ;
+  // winMessage.html(`${winner} is the winner`);
 });
+
+
+//fade in animation for winner winner message?
+// game over function
